1865e74381658df9904d02a2c12d48562f29f0a9
MaxmunZ <145468267+MaxmunZ@users.noreply.github.com>
2026-02-01 01:47:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,15 +14,28 @@ async def on_ready():
 
 @bot.command()
 async def owner(ctx):
-    await ctx.send("🚀 **About Owner**:\nOwner dari Stellar Universe adalah **MaxmunZ**. Membangun ekosistem bintang untuk semua!")
+    owner_text = (
+        "<a:Instagram:820584675008577546> ***Instagram*** <a:Arrow:820637327969746974> https://www.instagram.com/Lucky.praditya/\n"
+        "<a:Twitter:820587292363718656> ***Twitter*** <a:Arrow:820637327969746974> https://twitter.com/luckypraditya1?s=09\n"
+        "<a:YouTube:820587271199653918> ***YouTube*** <a:Arrow:820637327969746974> https://youtube.com/channel/UCcHVBAX0fSBOctB_tKPDk5Q\n"
+        "<a:Discord:820576752576888902> ***Discord*** <a:Arrow:820637327969746974> https://discord.gg/QEhHc6UBHH"
+    )
+    await ctx.send(owner_text)
 
 @bot.command()
 async def rules(ctx):
     rules_text = (
-        "📜 **Stellar Rules**:\n"
-        "1. Hormati sesama penghuni bintang.\n"
-        "2. Dilarang spam atau mengirim link berbahaya.\n"
-        "3. Ikuti instruksi Moderator."
+        "<a:1_:853534476344098847>" * 15 + "\n"
+        "                           <a:___:859993289113731123>     ***RULES*** <a:___:859993289113731123>\n"
+        "<a:2_:853534457457803265>" * 15 + "\n\n"
+        "**Kami memiliki aturan untuk mengakomodasi komunitas kami**\n\n"
+        "**1. Harap menghormati semua orang**\nAnda bebas untuk mengungkapkan pendapat yang berbeda tetapi juga menghormati orang lain.\n\n"
+        "**2. Hindari perilaku buruk**\nBersikaplah sopan. Itu saja. Argumen politik / agama juga boleh-boleh saja - tetapi bukan untuk merendahkan orang lain.\n\n"
+        "**3. Jangan Spam**\nJangan melakukan spam, seperti teks, gambar, emoji, reaksi emoji, audio yang mengganggu. Anda akan Dipenjara dan spam yang berkelanjutan akan di ban dari server.\n\n"
+        "**4. Jangan Beriklan**\nJangan mempromosikan sendiri server Anda atau server lain tanpa izin. termasuk memposting tautan undangan, baik sosial media atau pun link penjualan ilegal.\n\n"
+        "**5. Hindari drama & Jangan Sirkel2an**\nCobalah untuk tidak memulai drama yang tidak perlukan. Lapor admin / staff bila ada yang membuat kegaduhan\n\n"
+        "**6. Gunakan channel sesuai namanya**\nKami memiliki channel yang berbeda untuk tema bahasan diserver, jadi misalkan jika ingin mabar bisa ke mabar dan sesuai channel masing-masing.\n\n"
+        "**7. Dilarang menggunakan nickname toxic / foto profile yang dapat menimbulkan permasalahan**\nMohon menggunakan nick name dan profile picture SFW alias aman untuk semua umur tanpa mengandung unsur SARA atau mengundang pertikaian."
     )
     await ctx.send(rules_text)
 
