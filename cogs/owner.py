5be1be50905b151c673ce8553739028a08a4d686
Lucky Praditya <145468267+LucAetheryn@users.noreply.github.com>
2026-04-22 11:47:43 +0700

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -8,11 +8,16 @@ class Owner(commands.Cog):
     @commands.command()
     async def owner(self, ctx):
         embed = discord.Embed(
-            title="About Owner",
+            title="<:StellarUniverse:1468863371427450890> About Owner",
             color=0xBB86FC
         )
 
+        embed.add_field(name="Instagram", value="[Lucky.praditya](https://www.instagram.com/Lucky.praditya/)", inline=True)
+        embed.add_field(name="Twitter", value="[luckypraditya1](https://twitter.com)", inline=True)
+        embed.add_field(name="YouTube", value="Channel", inline=True)
+
         embed.set_thumbnail(url=self.bot.user.display_avatar.url)
+
         await ctx.send(embed=embed)
 
 async def setup(bot):
