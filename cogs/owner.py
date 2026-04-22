78d57403deb26256c6cde6c708000800704fd3ee
Lucky Praditya <145468267+LucAetheryn@users.noreply.github.com>
2026-04-22 11:47:14 +0700

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,19 +1,19 @@
 import discord
 from discord.ext import commands
 
-class Rules(commands.Cog):
+class Owner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def rules(self, ctx):
+    async def owner(self, ctx):
         embed = discord.Embed(
-            title="Server Rules",
-            description="Kami memiliki aturan untuk mengakomodasi komunitas kami",
+            title="About Owner",
             color=0xBB86FC
         )
 
+        embed.set_thumbnail(url=self.bot.user.display_avatar.url)
         await ctx.send(embed=embed)
 
 async def setup(bot):
-    await bot.add_cog(Rules(bot))
+    await bot.add_cog(Owner(bot))
