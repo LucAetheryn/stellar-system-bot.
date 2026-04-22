1e121399cab7b242f69796ebdc94fa13c6fd3f79
Lucky Praditya <145468267+LucAetheryn@users.noreply.github.com>
2026-04-22 11:43:12 +0700

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,24 +1,19 @@
 import discord
 from discord.ext import commands
 
-class Owner(commands.Cog):
+class Rules(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def owner(self, ctx):
+    async def rules(self, ctx):
         embed = discord.Embed(
-            title="<:StellarUniverse:1468863371427450890> About Owner",
+            title="Server Rules",
+            description="Kami memiliki aturan untuk mengakomodasi komunitas kami",
             color=0xBB86FC
         )
-
-        embed.add_field(name="Instagram", value="[Lucky.praditya](https://www.instagram.com/Lucky.praditya/)", inline=True)
-        embed.add_field(name="Twitter", value="[luckypraditya1](https://twitter.com)", inline=True)
-        embed.add_field(name="YouTube", value="Channel", inline=True)
-
-        embed.set_thumbnail(url=self.bot.user.display_avatar.url)
 
         await ctx.send(embed=embed)
 
 async def setup(bot):
-    await bot.add_cog(Owner(bot))
+    await bot.add_cog(Rules(bot))
