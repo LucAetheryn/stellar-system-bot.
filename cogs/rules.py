c9b0bd48a12026eb73985fe0b17e3ac1b3c7f37a
Lucky Praditya <145468267+LucAetheryn@users.noreply.github.com>
2026-04-22 11:35:41 +0700

    Create Rules cog with rules command
    Add a Rules cog with a command to display server rules.

---
+++ cogs/rules.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+class Rules(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def rules(self, ctx):
+        embed = discord.Embed(
+            title="Server Rules",
+            description="Isi Rules Server Kamu",
+            color=0xBB86FC
+        )
+
+        await ctx.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Rules(bot))
